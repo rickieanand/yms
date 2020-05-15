cc3565747d77498b66772f57c3e49f309d8856b3
Rickie Anand <mumbai85@gmail.com>
2020-05-15 14:54:35 +0530

    add blackboard

--- index.js
+++ index.js
@@ -16,39 +16,45 @@ app.use(router)
 
 
 io.on('connect', (socket) => {
-    socket.on('join', ({ name, room }, callback) => {
-      const { error, user } = addUser({ id: socket.id, name, room });
-        console.log('1 joined')
-      if(error) return callback(error);
-  
-      socket.join(room);
-  
-      socket.emit('message', { user: 'admin', text: `${name}, welcome to room ${room}.`});
-      socket.broadcast.to(room).emit('message', { user: 'admin', text: `${name} has joined!` });
-  
-      io.to(room).emit('roomData', { room: room, users: getUsersInRoom(room) });
-  
-      callback();
-    });
-  
-    socket.on('sendMessage', (message, callback) => {
-      const user = getUser(socket.id);
-  console.log('answer: ', user)
-      io.to(user.room).emit('message', { user: user.name, text: message });
-  
-      callback();
-    });
-  
-    socket.on('disconnect', () => {
-      const user = removeUser(socket.id);
-  
-      if(user) {
-        io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-      }
-    })
-  });
+	socket.on('join', ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
+		console.log('1 joined')
+		if (error) return callback(error);
+
+		socket.join(room);
+
+		socket.emit('message', { user: 'admin', text: `${name}, welcome to room ${room}.` });
+		socket.broadcast.to(room).emit('message', { user: 'admin', text: `${name} has joined!` });
+
+		io.to(room).emit('roomData', { room: room, users: getUsersInRoom(room) });
+
+		callback();
+	});
+
+	socket.on('sendMessage', (message, callback) => {
+		const user = getUser(socket.id);
+		console.log('answer: ', user)
+		io.to(user.room).emit('message', { user: user.name, text: message });
+
+		callback();
+	});
+
+	socket.on('mouse',
+		function (data) {
+			socket.broadcast.emit('mouse', data);
+		}
+	)
+
+	socket.on('disconnect', () => {
+		const user = removeUser(socket.id);
+
+		if (user) {
+			io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+			io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+		}
+	})
+});
 
 const PORT = process.env.PORT || 5000
 
-server.listen(PORT, ()=>{ console.log('Listening on ', PORT)})
+server.listen(PORT, () => { console.log('Listening on ', PORT) })
